36321d422fc3a627796b6fbc5e8be74bbae5743a
Phillip Sitbon <phillip.sitbon@intel.com>
2016-05-06 17:20:50 -0700

    kinetis isr: use a lower radio interrupt priority
    Signed-off-by: Phillip Sitbon <phillip.sitbon@intel.com>

--- fw/project/default/libcc/src/interface/kinetis/isr.c
+++ fw/project/default/libcc/src/interface/kinetis/isr.c
@@ -157,7 +157,7 @@ enum isr_pin cc_isr(cc_dev_t dev, enum isr_pin pin, enum isr_edge edge, isr_t is
         PORT_SetPinInterruptConfig(SYS_PORT_PORTN(cfg->port), cfg->pin, EDGE_MAP[edge]);
 
 //#if defined(configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY)
-        NVIC_SetPriority(SYS_PORT_IRQN(cfg->port), 1/*configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY-*//*CC_NUM_DEVICES+dev*/);
+        NVIC_SetPriority(SYS_PORT_IRQN(cfg->port), 5+dev/*configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY-*//*CC_NUM_DEVICES+dev*/);
 //#endif
 
         EnableIRQ(SYS_PORT_IRQN(cfg->port));
