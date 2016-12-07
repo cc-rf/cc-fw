37e31426cb1dead4c714881bc4ed6e358537200d
Phillip Sitbon <phillip.sitbon@intel.com>
2016-12-07 13:37:31 -0800

    remove fw_old

--- fw_old/project/usb_cdc_freertos/config.cmake
+++
@@ -1,5 +0,0 @@
-set(FW_BOARD fw_core_board_twr_k65f180m CACHE STRING "Firmware target board")
-set(FW_CPU fw_core_cpu_mk65f18 CACHE STRING "Firmware target CPU")
-#set(FW_BOARD fw_core_board_frdm_k22f CACHE STRING "Firmware target board")
-#set(FW_CPU fw_core_cpu_mk22f51212 CACHE STRING "Firmware target CPU")
-set(FW_USB_OSA fw_core_middleware_usb_osa_freertos CACHE STRING "Firmware USB OSA")
