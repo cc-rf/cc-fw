712acc9a024115aeafe9dd820047c7f340ec9ef6
Phillip Sitbon <phillip.sitbon@intel.com>
2018-11-02 05:05:39 -0700

    rdio: don't add i/o overhead to packet tx times
    Signed-off-by: Phillip Sitbon <phillip.sitbon@intel.com>

--- fw/lib/ccrf/rdio/util.c
+++ fw/lib/ccrf/rdio/util.c
@@ -498,5 +498,5 @@ u32 rdio_util_get_tx_time(rdio_t rdio, u32 len)
     // Note for later: with 4-ary modulation the preamble is still sent as 2-ary.
 
 
-    return 100/*I/O overhead*/ + ((1000000 * 8 * (len + overhead) /*+ (symbol_rate >> 1)*/) / symbol_rate);
+    return ((1000000 * 8 * (len + overhead) /*+ (symbol_rate >> 1)*/) / symbol_rate);
 }
