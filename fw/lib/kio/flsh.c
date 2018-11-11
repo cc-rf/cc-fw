c6a7ca0fe1d2cefb41c7ae01f32b6236aaffdf2f
Phillip Sitbon <phillip.sitbon@intel.com>
2018-11-10 20:30:35 -0800

    flash: copy by section and swap now working
    Signed-off-by: Phillip Sitbon <phillip.sitbon@intel.com>

--- fw/lib/kio/flsh.c
+++ fw/lib/kio/flsh.c
@@ -4,12 +4,13 @@
 #include <fsl_flash.h>
 #include <fsl_flash.h>
 #include <FreeRTOS.h>
-
+#include <task.h>
 
 static_assert(FSL_FEATURE_SIM_FCFG_HAS_PFLSH_SWAP, "FSL_FEATURE_SIM_FCFG_HAS_PFLSH_SWAP");
 
-
-#define OFFSET(x)           ((u32)(0x100000 + (u8 *)(x)))
+#define SIZE(begin, end)    ((u32)(end) - (u32)(begin))
+#define OFFSET(addr, amt)   ((u32 *)((amt) + (u8 *)(addr)))
+#define SWAP(x)             OFFSET(x, 0x100000)
 #define USER_FLASH_SIZE     FSL_FEATURE_FLASH_PFLASH_BLOCK_SECTOR_SIZE
 #define BLOCK_SIZE          FSL_FEATURE_FLASH_PFLASH_BLOCK_SECTOR_SIZE
 #define BLOCK_SIZE_32       (BLOCK_SIZE / sizeof(u32))
@@ -53,6 +54,8 @@ extern u32 __RAM_VECTOR_TABLE_SIZE_BYTES[];
 extern u32 __flash_header_begin[];
 extern u32 __flash_header_end[];
 
+extern u32 __all_rom_begin[];
+extern u32 __all_rom_end[];
 
 static user_flash_t *user_flsh;
 static user_flash_t *user_flsh_rom;
@@ -65,9 +68,16 @@ static user_flash_t user_flsh_ram __section(".user.base") = {
 static flash_config_t flash_config;
 static ftfx_config_t *ftfx_config;
 
-static status_t flsh_bcpy(u32 *src, u32 *end, u32 *dst);
 static status_t get_swap_state_config(ftfx_swap_state_config_t *swap_state_config)__used;
 
+static status_t flsh_scpy(const char name[], u32 begin[], u32 end[]);
+
+static void delay()
+{
+    #if DEBUG
+    for (u32 i = 0; i < 1000000; ++i) asm("");
+    #endif
+}
 
 status_t flsh_init(void)
 {
@@ -79,7 +89,7 @@ status_t flsh_init(void)
 
     user_flsh_rom = (user_flash_t *) __user_flash_init_base;
     user_flsh = (user_flash_t *) __user_flash_base;
-    user_flsh_othr = (user_flash_t *) OFFSET(__user_flash_base);
+    user_flsh_othr = (user_flash_t *) SWAP(__user_flash_base);
 
     wcpy(__user_flash_base, __user_flash_end, __user_ram_base);
 
@@ -94,12 +104,12 @@ status_t flsh_init(void)
 
     board_trace("");
     board_trace(  "section measurements:")
-    board_trace_f("  header        %u", (u32) __flash_header_end - (u32) __flash_header_begin);
-    board_trace_f("  .user <local> %u", (u32) __user_flash_end - (u32) __user_flash_base);
-    board_trace_f("  .user_rom     %u", (u32) __user_flash_init_end - (u32) __user_flash_init_base);
-    board_trace_f("  .fast_code    %u", (u32) __fast_text_end - (u32) __fast_text_begin);
-    board_trace_f("  .text         %u", (u32) __text_end - (u32) __text_begin);
-    board_trace_f("  .data         %u", (u32) __DATA_END - (u32) __DATA_ROM);
+    board_trace_f("  header        0x%05X-0x%05X    %u", __flash_header_begin, __flash_header_end, (u32) __flash_header_end - (u32) __flash_header_begin);
+    board_trace_f("  .user <local> 0x%05X-0x%05X    %u", __user_flash_base, __user_flash_end, (u32) __user_flash_end - (u32) __user_flash_base);
+    board_trace_f("  .user_rom     0x%05X-0x%05X    %u", __user_flash_init_base, __user_flash_init_end, (u32) __user_flash_init_end - (u32) __user_flash_init_base);
+    board_trace_f("  .fast_code    0x%05X-0x%05X    %u", __fast_text_begin, __fast_text_end, (u32) __fast_text_end - (u32) __fast_text_begin);
+    board_trace_f("  .text         0x%05X-0x%05X    %u", __text_begin, __text_end, (u32) __text_end - (u32) __text_begin);
+    board_trace_f("  .data         0x%05X-0x%05X    %u", __DATA_ROM, __DATA_END, (u32) __DATA_END - (u32) __DATA_ROM);
     board_trace("");
 
 
@@ -108,8 +118,7 @@ status_t flsh_init(void)
 
         wcpy(__user_flash_init_base, __user_flash_init_end, __user_ram_base);
 
-
-        if ((status = flsh_write((u32) __user_flash_base, (u32) __user_flash_end - (u32) __user_flash_base, __user_ram_base))) {
+        if ((status = flsh_ewrite(__user_flash_init_base, __user_flash_init_end, __user_flash_base))) {
             return status;
         } else {
             board_trace("done.");
@@ -127,76 +136,22 @@ status_t flsh_init(void)
 
     } else {
 
-
         board_trace("<invalid>");
-        board_trace_fr("copy flash header s=0x%X ... ", (u32)__flash_header_end - (u32)__flash_header_begin);
+
+        if ((status = flsh_erase(SWAP(__flash_header_begin), SWAP(__flash_header_end)))) return status;
+        if ((status = flsh_erase(SWAP(__user_flash_base),  SWAP(__user_flash_end)))) return status;
+        if ((status = flsh_erase(SWAP(__all_rom_begin),  SWAP(__all_rom_end)))) return status;
+
+        if ((status = flsh_scpy("header", __flash_header_begin, __flash_header_end))) return status;
+        if ((status = flsh_scpy(".user_rom", __user_flash_init_base, __user_flash_init_end))) return status;
+        if ((status = flsh_scpy(".fast_code", __fast_text_begin, __fast_text_end))) return status;
+        if ((status = flsh_scpy(".text", __text_begin, __text_end))) return status;
+        if ((status = flsh_scpy(".data", __DATA_ROM, __DATA_END))) return status;
+
+        if ((status = flsh_scpy(".user <local>", __user_flash_base, __user_flash_end))) return status;
         
-        if ((status = flsh_write(OFFSET(__flash_header_begin), (u32)__flash_header_end - (u32)__flash_header_begin, __flash_header_begin))) {
-            return status;
-        } else {
-            board_trace("done.");
-        }
+        delay();
 
-        if (1) {
-
-            board_trace_fr("copy text/data s=%u ... ",
-                           (u32) __DATA_END - (u32) __user_flash_base);
-
-            if ((status = flsh_bcpy(__user_flash_base, __DATA_END, (u32 *) OFFSET(__user_flash_base)))) {
-                return status;
-            } else {
-                board_trace("done.");
-            }
-
-        } else {
-
-
-            board_trace(  "copy:")
-
-            board_trace_fr("  header        %u ... ", (u32) __flash_header_end - (u32) __flash_header_begin);
-            if ((status = flsh_bcpy(__flash_header_begin, __flash_header_end, (u32 *) OFFSET(__flash_header_begin)))) {
-                return status;
-            } else {
-                board_trace("done.");
-            }
-
-            board_trace_fr("  .user <local> %u ... ", (u32) __user_flash_end - (u32) __user_flash_base);
-            if ((status = flsh_bcpy(__user_flash_base, __user_flash_end, (u32 *) OFFSET(__user_flash_base)))) {
-                return status;
-            } else {
-                board_trace("done.");
-            }
-
-            board_trace_fr("  .user_rom     %u ... ", (u32) __user_flash_init_end - (u32) __user_flash_init_base);
-            if ((status = flsh_bcpy(__user_flash_init_base, __user_flash_init_end, (u32 *) OFFSET(__user_flash_init_base)))) {
-                return status;
-            } else {
-                board_trace("done.");
-            }
-
-            board_trace_fr("  .fast_code    %u ... ", (u32) __fast_text_end - (u32) __fast_text_begin);
-            if ((status = flsh_bcpy(__fast_text_begin, __fast_text_end, (u32 *) OFFSET(__fast_text_begin)))) {
-                return status;
-            } else {
-                board_trace("done.");
-            }
-
-            board_trace_fr("  .text         %u ... ", (u32) __text_end - (u32) __text_begin);
-            if ((status = flsh_bcpy(__text_begin, __text_end, (u32 *) OFFSET(__text_begin)))) {
-                return status;
-            } else {
-                board_trace("done.");
-            }
-
-            board_trace_fr("  .data         %u ... ", (u32) __DATA_END - (u32) __DATA_ROM);
-            if ((status = flsh_bcpy(__DATA_ROM, __DATA_END, (u32 *) OFFSET(__DATA_ROM)))) {
-                return status;
-            } else {
-                board_trace("done.");
-            }
-
-        }
-        
         board_trace_r("swapping... ");
 
         if (user_flsh_othr->sanity == user_flsh_ram.sanity) {
@@ -205,6 +160,7 @@ status_t flsh_init(void)
             } else {
                 //board_trace("success.")
                 board_trace("success.\nreboot....\r\n\r\n\r\n");
+                delay();
                 //__BKPT(0);
                 NVIC_SystemReset();
             }
@@ -214,94 +170,91 @@ status_t flsh_init(void)
         }
     }
 
+    delay();
     return status;
 }
 
 
-static status_t flsh_bcpy(u32 *src, u32 *end, u32 *dst)
+static status_t flsh_scpy(const char name[], u32 begin[], u32 end[])
 {
-    status_t status = 1;
-    u32 size;
-    
-    while (src < end) {
-        size = (u8 *) end - (u8 *) src;
+    board_trace_fr("copy %s %u ... ", name, (u32) end - (u32) begin);
 
-        if (size > BLOCK_SIZE) {
-            size = BLOCK_SIZE;
-        }
+    status_t status = flsh_write(begin, end, (u32 *) SWAP(begin));
 
-        /*board_trace_fr("copying %u: [0x%X:0x%X]->0x%X... ", size, src, (u8 *) src + size, dst)*/
-
-        if ((status = flsh_write((u32)dst, size, src))) {
-            return status;
-        } else {
-            //board_trace("done.");
-        }
-        
-        src += size >> 2;
-        dst += size >> 2;
+    if (status) {
+        return status;
+    } else {
+        board_trace("done.");
     }
-    
+
     return status;
 }
 
-status_t flsh_write(u32 begin, size_t size, u32 *data)
+status_t flsh_erase(u32 begin[], u32 end[])
 {
-    status_t status;
-    size_t erase_size = size;
+    size_t size = (u32) end - (u32) begin;
 
-    if (size % 4) return 1;
+    if (size % 4) return -101;
+    if (size % 16) size = size + 16 - (size % 16);
 
-    if (size % 16) erase_size = size + 16 - (size % 16);
-
-    portDISABLE_INTERRUPTS();
-
-    status = FLASH_Erase(&flash_config, begin, erase_size, kFLASH_ApiEraseKey);
+    status_t status = FLASH_Erase(&flash_config, (u32)begin, size, kFLASH_ApiEraseKey);
 
     if (status) {
         board_trace_f("erase fail: s=%li", status);
+    }
 
+    return status;
+}
+
+status_t flsh_ewrite(u32 *begin, u32 *end, u32 dest[])
+{
+    status_t status = flsh_erase(dest, OFFSET(dest, SIZE(begin, end)));
+
+    if (!status) {
+        return flsh_write(begin, end, dest);
+    }
+
+    return status;
+}
+
+status_t flsh_write(u32 *begin, u32 *end, u32 dest[])
+{
+    status_t status = FLASH_Program(&flash_config, (u32) dest, (u8 *) begin, SIZE(begin, end));
+
+    if (status != kStatus_FLASH_Success) {
+        board_trace_f("write fail: %li", status);
     } else {
-        status = FLASH_Program(&flash_config, begin, (u8 *) data, size);
-
-        if (status != kStatus_FLASH_Success) {
-            board_trace_f("write fail: %li", status);
-        } else {
-            u32 *dst = (u32 *) begin;
-            u32 *end = (u32 *) (begin + size);
-
-            while (dst < end) {
-                if (*dst != *data) {
-                    board_trace_f("verify fail: wrote=0x%X@0x%X != read=0x%X@0x%X", *dst, dst, *data, data);
-                    return 2;
-                }
-
-                ++dst;
-                ++data;
+        while (begin < end) {
+            if (*begin++ != *dest++) {
+                board_trace_f("verify fail: wrote=0x%X@0x%X != read=0x%X@0x%X", *begin, begin, *dest, dest);
+                return -2;
             }
         }
     }
 
-    portENABLE_INTERRUPTS();
+
     return status;
 }
+
 
 
 status_t flsh_user_cmit(void)
 {
     status_t status;
 
-    portDISABLE_INTERRUPTS();
+    taskENTER_CRITICAL();
+    //portDISABLE_INTERRUPTS();
 
     boot_clock_run();
     itm_init();
 
-    status = flsh_bcpy(__user_ram_base, __user_ram_end, __user_flash_base);
+    status = flsh_ewrite(__user_ram_base, __user_ram_end, __user_flash_base);
 
     boot_clock_run_hs_oc();
     itm_init();
 
-    portENABLE_INTERRUPTS();
+    //portENABLE_INTERRUPTS();
+    taskEXIT_CRITICAL();
 
     return status;
 }
--- fw/lib/kio/include/kio/flsh.h
+++ fw/lib/kio/include/kio/flsh.h
@@ -3,9 +3,11 @@
 #include <usr/type.h>
 #include <fsl_common.h>
 
-#define FLASH_SANITY        0xF10BAEBE
+#define FLASH_SANITY        0xFE0BAEBE
 #define FLASH_VERSION       0xBEDFECE5
 
 status_t flsh_init(void) __fast_code;
-status_t flsh_write(u32 begin, size_t size, u32 *data) __fast_code;
-status_t flsh_user_cmit(void);
+status_t flsh_erase(u32 begin[], u32 end[]) __fast_code;
+status_t flsh_ewrite(u32 *begin, u32 *end, u32 dest[]) __fast_code;
+status_t flsh_write(u32 *begin, u32 *end, u32 dest[])__fast_code;
+status_t flsh_user_cmit(void) __fast_code;
